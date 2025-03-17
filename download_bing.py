54996b0c0d39778754bca503808de1f8169801c1
Vladimir Krasnoselskikh <v.krasnoseslkikh.im@gmail.com>
2025-03-17 21:58:03 +0400

    Add uv

--- download_bing.py
+++ download_bing.py
@@ -8,6 +8,8 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 from string import Formatter, Template
 
+DEFAULT_PATH_DOWNLOAD = (Path.home() / 'Pictures'/ 'bing-wallpapers')
+
 
 @dataclasses.dataclass
 class ImageInfo:
@@ -19,43 +21,41 @@ class ImageInfo:
 
 class ApiBingImage(httpx.Client):
     def __init__(self):
-        super().__init__(base_url='https://www.bing.com')
+        super().__init__(base_url="https://www.bing.com")
 
     def get_image_info(self, days_before: int = None) -> ImageInfo:
         if days_before is None:
             days_before = 0
 
-        resp = self.get('/HPImageArchive.aspx', params={
-            'format': 'xml',
-            'idx': days_before,
-            'n': 1
-        })
+        resp = self.get(
+            "/HPImageArchive.aspx", params={"format": "xml", "idx": days_before, "n": 1}
+        )
         tree = ET.fromstring(resp.text)
-        image_data = tree.find('image')
-        url_base: str = image_data.find('urlBase').text
-        date_str = image_data.find('startdate').text
+        image_data = tree.find("image")
+        url_base: str = image_data.find("urlBase").text
+        date_str = image_data.find("startdate").text
         return ImageInfo(
-            id=url_base.replace('/th?id=', ''),
-            start_date=datetime.datetime.strptime(date_str, '%Y%m%d').date(),
+            id=url_base.replace("/th?id=", ""),
+            start_date=datetime.datetime.strptime(date_str, "%Y%m%d").date(),
             url_base=url_base,
-            title=image_data.find('copyright').text
+            title=image_data.find("copyright").text,
         )
 
     def get_content_image(self, image_info: ImageInfo) -> bytes:
-        _format = '_UHD.jpg'
+        _format = "_UHD.jpg"
         _url_image = image_info.url_base + _format
         resp = self.get(_url_image)
         return resp.content
 
 
-def download_image(base_download_path: str, days_before: int = None) -> Path:
+def download_image(base_download_path: Path, days_before: int = None) -> Path:
     api = ApiBingImage()
     info = api.get_image_info(days_before=days_before)
-    picture_name = info.id + '.jpg'
+    picture_name = info.id + ".jpg"
     image_path = base_download_path / picture_name
     print(f"Download image: {info.title}. Picture_name: {picture_name}")
 
-    with open(image_path, 'wb') as f:
+    with open(image_path.absolute(), "wb") as f:
         f.write(api.get_content_image(info))
     return image_path
 
@@ -74,14 +74,20 @@ def set_image_to_mac_monitor(monitor: int, file_path):
 
 
 def cli():
-    parser = argparse.ArgumentParser(description='Download images from Bing.')
+    parser = argparse.ArgumentParser(description="Download images from Bing.")
 
-    parser.add_argument('--days-before', type=int,
-                        default=0,
-                        help='Days before image was in Bing')
-    parser.add_argument('--base-path', type=Path, default=Path(__file__).parent, help='Path for downloading images')
+    parser.add_argument(
+        "--days-before", type=int, default=0, help="Days before image was in Bing"
+    )
+    parser.add_argument(
+        "--base-path",
+        type=Path,
+        default=DEFAULT_PATH_DOWNLOAD,
+        help="Path for downloading images",
+    )
 
-    parser.add_argument('--mac-monitor', type=int, help='Set image to Mac monitor')
+    parser.add_argument("--mac-monitor", type=int, default=1, help="Set image to Mac monitor")
+
 
     args = parser.parse_args()
 
@@ -90,5 +96,5 @@ def cli():
         set_image_to_mac_monitor(args.mac_monitor, image_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
